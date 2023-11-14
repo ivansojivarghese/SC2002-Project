b76819cb8ee750008c6b07df899e70b3a22d0872
Rabbitson2001 <67959899+Rabbitson2001@users.noreply.github.com>
2023-11-15 01:22:23 +0800

    suggestion handler class added

--- cams/SuggestionHandler/SuggestionViewerUI.java
+++ cams/SuggestionHandler/SuggestionViewerUI.java
@@ -1,4 +1,42 @@
 package cams.SuggestionHandler;
 
+import cams.PostTypes.Post;
+import cams.User;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class SuggestionViewerUI {
+    public void displayMenu(User user){
+        Post currentPost;
+        Scanner userInput = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Select an action: ");
+            System.out.println("1. View my enquiries");
+            System.out.println("-1. Back");
+            choice = userInput.nextInt();
+
+            switch (choice){
+                case 1:
+                    view(user);
+                    break;
+                case 2:
+                    break;
+            }
+        } while(choice != -1);
+    }
+
+    public int view(User user){
+        Post currentPost;
+        System.out.println("My Camp Enquiries: ");
+        List<Post> myEnquiries = user.getEnquiries();
+        for (int i = 0; i < myEnquiries.size(); i++) {
+            currentPost = myEnquiries.get(i);
+            System.out.println(i + ": ");
+            currentPost.displayContent();
+            System.out.println("__________________________");
+        }
+        return 1;
+    }
 }
